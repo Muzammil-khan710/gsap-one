43961bb8c9e7ba57c9526ee5ca8df63c9d1fc47d
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-01-19 15:07:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const select = (e) => document.querySelector(e);
+
+const canvas = select("#canvas-el");
+const context = canvas.getContext("2d");
+canvas.height = 810;
+canvas.width = 1420;
+
+const frameCount = 64;
+
+const currentFrame = (index) =>
+  `https://www.apple.com/105/media/us/airpods-pro/2022/d2deeb8e-83eb-48ea-9721-f567cf0fffa8/anim/hero/large/${(
+    index + 1
+  )
+    .toString()
+    .padStart(4, "0")}.png`;
+
+const images = [];
+const airpods = {
+  frame: 0,
+};
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  images.push(img);
+}
+
+gsap.to(airpods, {
+  frame: frameCount - 1,
+  snap: "frame",
+  scrollTrigger: {
+    scrub: 0.5,
+    markers:true
+  },
+  onUpdate: render, // use animation onUpdate instead of scrollTrigger's onUpdate
+});
+
+images[0].onload = render;
+
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[airpods.frame], 0, 0);
+}
+
+gsap.fromTo('.banner', {opacity: 0}, {duration:2, opacity:1})
+
+gsap.fromTo('.banner-fv', {scale:0.7}, {duration:2, scale:1})
+
+gsap.fromTo('.banner-fv-up', {y:10}, {duration:2, y:-10})
+
+gsap.to('.banner-fv-down', {duration:2, y:100})
+
+gsap.to('.banner-rebuilt', {
+  scrollTrigger: {
+    markers:true
+  }
+})
